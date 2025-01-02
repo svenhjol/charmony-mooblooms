2831bdd20b5bac026d5a2aa5f6dcf7c8b9f71cdd
Sven H <sven.hjol@protonmail.com>
2025-01-02 22:06:47 +0000

    Refactor for Charmony 1.19.0. 1.1.0

--- src/main/java/svenhjol/charmony/mooblooms/common/features/mooblooms/Advancements.java
+++ src/main/java/svenhjol/charmony/mooblooms/common/features/mooblooms/Advancements.java
@@ -2,7 +2,7 @@ package svenhjol.charmony.mooblooms.common.features.mooblooms;
 
 import net.minecraft.world.entity.player.Player;
 import svenhjol.charmony.core.base.Setup;
-import svenhjol.charmony.core.helper.AdvancementHelper;
+import svenhjol.charmony.core.helpers.AdvancementHelper;
 
 public final class Advancements extends Setup<Mooblooms> {
     public Advancements(Mooblooms feature) {
